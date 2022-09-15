93bd1b09e32b96b2c6081e8c375778ade534607e
UsielOM <usiel31197@outlook.es>
2022-09-15 12:01:10 -0500

    Estructura basica de lit

---
+++ src/my-taimer.js
@@ -0,0 +1,16 @@
+import { LitElement, html } from 'lit-element';
+
+export class MyTaimer extends LitElement {
+
+
+    static get properties() {
+        return {
+
+        };
+    }
+
+    render() {
+        return html ``;
+    }
+}
+customElements.define('my-taimer', MyTaimer);
